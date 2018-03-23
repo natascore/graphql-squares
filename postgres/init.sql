b23b341760a31c36efec8eada316a328a993370b
Daniel Paschke <paschdan@gmail.com>
2018-03-23 11:22:34 +0100

    add postgres initialization

---
+++ postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS "squares" ("id"   SERIAL , "status" VARCHAR(255), "created_at" TIMESTAMP WITH TIME ZONE NOT NULL, "updated_at" TIMESTAMP WITH TIME ZONE NOT NULL, PRIMARY KEY ("id"));
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'free','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'free','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'reserved','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'reserved','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'sold','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'sold','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
+ INSERT INTO "squares" ("id","status","created_at","updated_at") VALUES (DEFAULT,'free','2018-03-23 10:09:15.174 +00:00','2018-03-23 10:09:15.174 +00:00');
